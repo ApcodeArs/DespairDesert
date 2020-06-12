fcdacb50f7fe358a9393a1e7a4ff731fae420c5d
Ars <arseniy59@yandex.ru>
2020-06-12 14:27:41 +0300

    add simple tank movement

--- Assets/Scenes/Scripts/Tank.cs
+++ Assets/Scenes/Scripts/Tank.cs
@@ -4,15 +4,48 @@ using UnityEngine;
 
 public class Tank : MonoBehaviour
 {
-    // Start is called before the first frame update
+    private Vector3 _tankRotation;
+
+    //todo add to Scriptable Object
+    private const float Speed = 5.0f;
+    private const float RotationSpeed = 2.5f;
+
     void Start()
     {
-        
+        _tankRotation = transform.rotation.eulerAngles;
     }
 
-    // Update is called once per frame
-    void Update()
+
+    void FixedUpdate()
     {
-        
+        Move();
+    }
+
+    private void Move()
+    {
+        #region Rotation
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            _tankRotation.z += RotationSpeed;
+        }
+        if (Input.GetKey(KeyCode.RightArrow))
+        {
+            _tankRotation.z -= RotationSpeed;
+        }
+
+        transform.rotation = Quaternion.Euler(_tankRotation);
+        #endregion
+
+        //
+        #region Shifting
+        if (Input.GetKey(KeyCode.UpArrow))
+        {
+            transform.position += transform.rotation * Vector3.up * Time.deltaTime * Speed;
+        }
+        if (Input.GetKey(KeyCode.DownArrow))
+        {
+            transform.position += transform.rotation * Vector3.down * Time.deltaTime * Speed;
+        }
+        #endregion
     }
 }
