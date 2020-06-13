0714017fbf1ac6e751cb8b5f4904ee8c618869e2
Ars <arseniy59@yandex.ru>
2020-06-13 12:53:14 +0300

    start work with enimies

---
+++ Assets/Scripts/Enemies/BaseEnemy.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Sirenix.OdinInspector;
+using UnityEngine;
+
+public abstract class BaseEnemy : MonoBehaviour
+{
+    //todo add to Scriptable Object
+
+    [BoxGroup("BaseEnemy")]
+    public const int EnemiesCount = 10;
+   
+    [BoxGroup("BaseEnemy")]
+    public string Name;
+
+    [BoxGroup("BaseEnemy")]
+    public float Health;
+    [BoxGroup("BaseEnemy")]
+    public float Protection;
+
+    [BoxGroup("BaseEnemy")]
+    public float Speed;
+    [BoxGroup("BaseEnemy")]
+    public float Damage;
+    
+    private void Awake()
+    {
+        AwakeEnemy();
+    }
+
+    private void FixedUpdate()
+    {
+        FixedUpdateEnemy();
+    }
+
+    public abstract void AwakeEnemy();
+
+    //public abstract void Init(GameObject parent);
+
+    public abstract void FixedUpdateEnemy();
+}
---
+++ Assets/Scripts/Enemies/EnimyType2.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnimyType2 : BaseEnemy
+{
+    public override void AwakeEnemy()
+    {
+
+    }
+
+    public override void FixedUpdateEnemy()
+    {
+
+    }
+}
