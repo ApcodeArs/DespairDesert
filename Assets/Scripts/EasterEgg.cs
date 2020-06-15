7e3ed781193814cac7ad495703d35dfa2a5da610
Ars <arseniy59@yandex.ru>
2020-06-16 00:13:16 +0300

    Add something interesting :P

---
+++ Assets/Scripts/EasterEgg.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Random = UnityEngine.Random;
+
+public class EasterEgg : MonoBehaviour
+{
+    private readonly TimeSpan _startSiestaHour = new TimeSpan(14,0,0); 
+    private readonly TimeSpan _endSiestaHour = new TimeSpan(16, 0, 0);
+
+    private const float EntryThreshold = 0.5f;
+
+    public SpriteRenderer SombreroPrefab;
+
+    void Awake()
+    {
+        if (Random.value > EntryThreshold)
+            return;
+
+        //add to check TimeSpan(15, 0, 0)
+        var currentTime = DateTime.Now.TimeOfDay;
+
+        if (currentTime > _startSiestaHour && currentTime < _endSiestaHour)
+        {
+            Debug.Log("Siesta!!!");
+            Instantiate(SombreroPrefab, GameObject.Find("Tank").transform);
+        }
+    }
+}
